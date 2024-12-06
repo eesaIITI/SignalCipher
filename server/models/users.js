5a51359afa80e0210f9bee9fddd02169e5c59d13
Naveen <146852997+NaveenSh7@users.noreply.github.com>
2024-12-06 16:16:15 +0530

    Update users.js

--- server/models/users.js
+++ server/models/users.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 const UsersSchema = new mongoose.Schema({
     UserEmail: { type: String, required: true, unique: true },
     UserName: { type: String },
-    QnsSolved: { type: [Number] }, // Array of numbers
+    Qns_Solved: { type: [Number] }, // Array of numbers
+    CurrQn : {type : Number,default: 0},
 });
 
 const UsersModel = mongoose.model('users' , UsersSchema);
