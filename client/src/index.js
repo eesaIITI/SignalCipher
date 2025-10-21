7fd0fd482d91be42224c8192c29f31dfd6d86f91
piyush-raj-07 <piyushraj07092005@gmail.com>
2025-10-22 00:24:36 +0530

    solve error

--- client/src/index.js
+++ client/src/index.js
@@ -7,15 +7,14 @@ import App from './App.js'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-  <Auth0Provider
-  
-    domain="dev-u8nmi1cq4hyoy10r.us.auth0.com"
-    clientId="vv8o4LY6611SMBwV5JVYSOIV5aOJES04"
-    authorizationParams={{
-      redirect_uri: "http://localhost:3000"
-    }}
-  >
-    <App />
-  </Auth0Provider>,
+ <Auth0Provider
+  domain="dev-u8nmi1cq4hyoy10r.us.auth0.com"
+  clientId="vv8o4LY6611SMBwV5JVYSOIV5aOJES04"
+  authorizationParams={{
+    redirect_uri: window.location.origin,
+  }}
+>
+  <App />
+</Auth0Provider>
   </StrictMode>,
 );
